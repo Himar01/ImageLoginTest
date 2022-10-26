b1b370e6ae443c47438b95b7cc4faf1a60dc31ef
himar01 <himarmoreno01@gmail.com>
2022-10-26 17:02:10 +0100

    Almost done, but some errors make it not to work

--- app/src/main/java/es/ulpgc/moreno101/himar/imagelogintest/DogsResponse.kt
+++ app/src/main/java/es/ulpgc/moreno101/himar/imagelogintest/DogsResponse.kt
@@ -10,25 +10,4 @@ import retrofit2.http.Url
 data class DogsResponse (@SerializedName("status") var status:String,
 @SerializedName("message")var images: List<String>){
 
-    interface APIService {
-        @GET
-        fun getCharacterByName(@Url url:String): Res<DogsResponse>
-
-    }
-
-    private fun getRetrofit(): Retrofit {
-        return Retrofit.Builder()
-            .baseUrl("https://dog.ceo/api/breed/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }
-    private fun searchByName(query: String) {
-        doAsync{
-            val call = getRetrofit().create(APIService::class.java).getCharacterByName("$query/images").execute()
-            val puppies = call.body() as DogsResponse
-            uiThread{
-
-            }
-        }
-    }
 }
--- app/src/main/java/es/ulpgc/moreno101/himar/imagelogintest/MainActivity.kt
+++ app/src/main/java/es/ulpgc/moreno101/himar/imagelogintest/MainActivity.kt
@@ -4,12 +4,23 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import com.squareup.picasso.Picasso
 import es.ulpgc.moreno101.himar.imagelogintest.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener{
     private lateinit var binding: ActivityMainBinding
+    private val dogImages = mutableListOf<String>()
+    private var initDogValue:Int=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -17,10 +28,49 @@ class MainActivity : AppCompatActivity() {
         var currentTime: Date = Calendar.getInstance().time;
         var prefs = Prefs(applicationContext)
         binding.lastLogin.text = prefs.name
+        binding.searchView.setOnQueryTextListener(this)
         prefs.name= currentTime.toString()
-    }
-}
 
+    }
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        if(!query.isNullOrEmpty()){
+            searchByName(query.lowercase())
+        }
+        return true
+    }
+    override fun onQueryTextChange(newText: String?): Boolean {
+        return true
+    }
+
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://dog.ceo/api/breed/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+    private fun searchByName(query: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val call: Response<DogsResponse> = getRetrofit().create(APIService::class.java).getDogsByBreeds("$query/images")
+            val puppies:DogsResponse? = call.body()
+            runOnUiThread(){
+                if(call.isSuccessful){
+                    val images = puppies?.images ?: emptyList()
+                    dogImages.clear()
+                    dogImages.addAll(images)
+                    Picasso.get().load(dogImages[initDogValue]).into(binding.dogImage)
+
+                }else{
+                    //show error
+                }
+            }
+
+        }
+
+
+}
+    private fun showError() {
+        Toast.makeText(this,"Ha ocurrido un error", Toast.LENGTH_SHORT).show()
+    }
 class Prefs (context: Context){
     val PREFS_NAME = "es.ulpgc.moreno101.himar.imagelogintest"
     val SHARED_NAME = "shared_name"
@@ -30,3 +80,11 @@ class Prefs (context: Context){
         get() = prefs.getString(SHARED_NAME, "")
         set(value) = prefs.edit().putString(SHARED_NAME, value).apply()
 }
+
+
+
+}
+
+
+
+
